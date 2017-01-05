b0c790683ad9a50c648989302dc5fa0e8ce4be41
Lambros Petrou <lambrospetrou@gmail.com>
2017-01-05 22:57:43 +0000

    Removed unneded variable from docker build command.

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ set -ex
 # use this if you have GO in your system
 #CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo --ldflags="-s" -o app-builder
 # or this if you do not have Go locally and prefer to use a docker image for building too
-docker run --rm -it -v "$GOPATH":/go -v "$(pwd)":/app -e "GOPATH=/go" -w /app golang:1.7 sh -c 'CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo --ldflags="-s" -o application-main'
+# Map the current directory (root of our Go app) to the /app path which will be the working directory
+docker run --rm -it -v "$(pwd)":/app -w /app golang:1.7 sh -c 'CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo --ldflags="-s" -o application-main'
 
 # build the docker image with just the built binary
 docker build -t lp/application-main -f Dockerfile .
